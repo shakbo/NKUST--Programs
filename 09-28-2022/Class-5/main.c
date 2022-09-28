b68edaafbf7a1061caf918ff6962acf8fbdca033
RÅBÅHAJ <speciers63@gmail.com>
2022-09-28 10:20:24 +0800

    Update Class-5 for tips

--- 09-28-2022/Class-5/main.c
+++ 09-28-2022/Class-5/main.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 int main(void) {
-  int x, y;
+  int x, y; //宣告 x 和 y 用於紀錄使用者輸入的值。
 
-  printf("請輸入蛋的數量 > ");
-  scanf("%d", &x);
+  printf("請輸入蛋的數量 > "); //輸出函數，提示使用者輸入。
+  scanf("%d", &x); //取得使用者輸入的值。
 
-  printf("請輸入幾顆蛋為一盒 > ");
-  scanf("%d", &y);
-  printf("\n");
+  printf("請輸入幾顆蛋為一盒 > "); //輸出函數，提示使用者輸入。
+  scanf("%d", &y); //取得使用者輸入的值。
+  printf("\n"); //換行
 
-  int Box = x/y;
-  int LeftEggs = x%y;
+  int Box = x/y; //除法求整數
+  int LeftEggs = x%y; //除法求餘數
 
-  printf("共有%d盒蛋。\n剩下%d個蛋。\n\n", Box, LeftEggs);
+  printf("共有%d盒蛋。\n剩下%d個蛋。\n\n", Box, LeftEggs); //輸出函數，顯示計算出的值。
 
-  system("pause");
+  system("pause"); //暫停程式
 }
