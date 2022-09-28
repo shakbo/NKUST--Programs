c160c8ff7c7eb47d9241074910890704426eb38d
RÅBÅHAJ <speciers63@gmail.com>
2022-09-28 10:20:44 +0800

    Add tips

--- 09-14-2022/Class-1/main.c
+++ 09-14-2022/Class-1/main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 int main() {
-    printf("歡迎來到C語言的世界!\n");
-    printf("我的第一個程式!\n");
-    system("pause");
+    printf("歡迎來到C語言的世界!\n"); //輸出函數
+    printf("我的第一個程式!\n"); //輸出函數
+    system("pause"); //輸出函數
     //return 0;
 }
