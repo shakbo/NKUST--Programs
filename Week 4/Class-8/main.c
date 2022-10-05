3993395fe818ea9f30903d7f2237de72b6983ede
RÅBÅHAJ <speciers63@gmail.com>
2022-10-05 11:30:43 +0800

    Add find minimum

--- Week 4/Class-8/main.c
+++ Week 4/Class-8/main.c
@@ -3,7 +3,8 @@
 
 int main() {
   int a, b, c, d; //宣告整數 a b c，用於儲存使用者輸入及比較大小。
-  int max;
+  int max = -2147483648;
+  int min = 2147483647;
 
   printf("請輸入 a 的值 > "); //輸出函數，提示使用者輸入。
   scanf("%d", &a); //取得使用者輸入的值，並存於 a。
@@ -22,7 +23,14 @@ int main() {
   if (c > max) max = c; //如果 c 比 max 大，用 c 的值取代 max 的值。
   if (d > max) max = d; //如果 d 比 max 大，用 d 的值取代 max 的值。
 
-  printf("最大值為 %d", max);
+  if (a < min) min = a; //如果 a 比 min 大，用 a 的值取代 min 的值。
+  if (b < min) min = b; //如果 b 比 min 大，用 b 的值取代 min 的值。
+  if (c < min) min = c; //如果 c 比 min 大，用 c 的值取代 min 的值。
+  if (d < min) min = d; //如果 d 比 min 大，用 d 的值取代 min 的值。
+
+
+  printf("最大值為 %d\n", max);
+  printf("最小值為 %d", min);
 
   printf("\n\n"); //換行。
   system("pause"); //暫停程式，防止視窗提前關閉。
