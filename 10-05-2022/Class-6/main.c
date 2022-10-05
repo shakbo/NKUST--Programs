b26ccdeee9882d56b0a992961100005d87ede747
RÅBÅHAJ <speciers63@gmail.com>
2022-10-05 10:02:59 +0800

    Replace guessing one to real class 6

--- 10-05-2022/Class-6/main.c
+++ 10-05-2022/Class-6/main.c
@@ -3,36 +3,27 @@
 
 int main() {
   int a, b, c; //宣告整數 a b c，用於儲存使用者輸入及比較大小。 
-
+  
   printf("請輸入 a 的值 > "); //輸出函數，提示使用者輸入。
   scanf("%d", &a); //取得使用者輸入的值，並存於 a。
 
   printf("請輸入 b 的值 > "); //輸出函數，提示使用者輸入。
-  scanf("%d", &b); //取得使用者輸入的值，並存於 b。
+  scanf("%d", &b); //取得使用者輸入的值，並存於 a。
 
   printf("請輸入 c 的值 > "); //輸出函數，提示使用者輸入。
-  scanf("%d", &c); //取得使用者輸入的值，並存於 c。
+  scanf("%d", &c); //取得使用者輸入的值，並存於 a。
 
-  if(a > b) { //判斷 a 是否大於 b。
-    if(b > c) { //判斷 b 是否大於 c。
-      printf("a > b > c");
-    } else { //如果 b 小於 c。
-      if(c > a) { //判斷 c 是否大於 a。
-        printf("c > a > b");
-      } else {
-        printf("a > c > b");
-      }
+  if (a > b) { //判斷 a 是否大於 b。
+    if (b > c) printf("a > b > c"); //判斷 b 是否大於 c。
+    else { //如果 b 小於 c。
+      if (a > c) printf("a > c > b"); //判斷 a 是否大於 c。
+      else printf("c > a > b");
     }
   } else { //如果 b 小於 a。
-    if(b > c) { //判斷 b 是否大於 c。
-      if(c > a) { //判斷 c 是否大於 a。
-        printf("b > c > a");
-      } else { //如果 c 小於 a。
-        printf("b > a > c");
-      }
-    } else { //如果 b 小於 c。
-      printf("c > b > a");
-    }
+    if (b > c) { //判斷 b 是否大於 c。
+      if (a > c) printf("b > a > c"); //判斷 a 是否大於 c。
+      else printf("b > c > a");
+    } else printf("c > b > a");
   }
 
   printf("\n\n"); //換行。
