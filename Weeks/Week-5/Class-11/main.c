10f8816e5849f149e57cb587b6902166d257b242
RÅBÅHAJ <speciers63@gmail.com>
2022-10-12 11:47:31 +0800

    Update new codes

--- Weeks/Week-5/Class-11/main.c
+++ Weeks/Week-5/Class-11/main.c
@@ -4,10 +4,15 @@
 
 int main() {
   int year;
+  int leapYearsCounter = 0;
 
   for(year=1000; year<=3000; year++) {
-    if((year%400==0) || ((year%4)==0 && (year%100)!=0)) printf("%d 是閏年\n", year);
+    if((year%400==0) || ((year%4)==0 && (year%100)!=0)) {
+      printf("%d 是閏年\n", year);
+      leapYearsCounter++;
+    }
   }
+  printf("共有 %d 個閏年", leapYearsCounter);
 
   printf("\n\n"); //換行。
   system("pause"); //暫停程式，防止視窗提前關閉。
