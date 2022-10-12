25a27ac67bff100c4e44029dd4900cc37e4f14a6
RÅBÅHAJ <speciers63@gmail.com>
2022-10-12 11:42:28 +0800

    Add new class

--- Weeks/Week-5/Class-11/main.c
+++ Weeks/Week-5/Class-11/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main() {
+  int year;
 
+  for(year=1000; year<=3000; year++) {
+    if((year%400==0) || ((year%4)==0 && (year%100)!=0)) printf("%d 是閏年\n", year);
+  }
+
+  printf("\n\n"); //換行。
+  system("pause"); //暫停程式，防止視窗提前關閉。
 }
