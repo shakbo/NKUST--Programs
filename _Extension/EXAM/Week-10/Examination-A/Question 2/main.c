22704e537ed34a23d3c2405c742786f623eae49f
RoboStuido <speciers63@gmail.com>
2022-11-23 10:22:25 +0800

    Fix error

--- _Extension/EXAM/Week-10/Examination-A/Question 2/main.c
+++ _Extension/EXAM/Week-10/Examination-A/Question 2/main.c
@@ -3,18 +3,17 @@
 
 int main() {
   int a = 0, b = 0, c = 0, d = 0, e = 0;
-  int min = 0, max = 0;
 
   printf("Please input 5 values: ");
   scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
 
-  if(min > a) min = a;
+  int min = a, max = a;
+
   if(min > b) min = b;
   if(min > c) min = c;
   if(min > d) min = d;
   if(min > e) min = e;
 
-  if(max < a) max = a;
   if(max < b) max = b;
   if(max < c) max = c;
   if(max < d) max = d;
