43185df634414c50ef665a1a2d979f0b231677b6
RoboStuido <speciers63@gmail.com>
2022-12-08 18:51:33 +0800

    fix bug

--- Weeks/Week-12/AfterClass/Question-3/main.c
+++ Weeks/Week-12/AfterClass/Question-3/main.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 
 int main() {
-  double N=1.0;
+  int original_price=0;
+  double calculated_price=0.0;
 
   printf("請輸入金額: ");
-  scanf("%lf", &N);
+  scanf("%d", &original_price);
 
-  if(N >= 100.0) N *= 0.95;
-  else if(N >= 1000.0) N *= 0.9;
-  else if(N >= 10000.0) N *= 0.85;
+  if((original_price >= 100) && (original_price < 1000)) calculated_price = original_price * 0.95;
+  else if((original_price >= 1000) && (original_price < 10000)) calculated_price = original_price * 0.9;
+  else if(original_price >= 10000) calculated_price = original_price * 0.85;
 
-  printf("應付價格: %lf", N);
-  
+  printf("應付價格: %lf", calculated_price);  
 
   printf("\n\n");
   system("pause");
