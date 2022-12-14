6904c0baf5a512f77f554419c8f846a0dc161b01
RoboStuido <speciers63@gmail.com>
2022-12-14 09:59:52 +0800

    rename variables

--- Weeks/Week-14/Class-18/main.c
+++ Weeks/Week-14/Class-18/main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int add_from_1_to_given_num(int num) {
-  int sum=0;
+int add_from_1_to_given_number(int target) {
+  int summary=0;
 
-  for(int i=0; i<=num; i++) {
-    sum += i;
+  for(int i=0; i<=target; i++) {
+    summary += i;
   }
   
-//  sum = ((1 + num) * num) / 2; // 公式解
+//  summary = ((1 + target) * target) / 2; // 公式解
 
-  return sum;
-}
+  return summary;
+} 
 
 int main() {
-  int num, result;
+  int number, result;
 
-  printf("請輸入num: ");
-  scanf("%d", &num);
+  printf("請輸入number: ");
+  scanf("%d", &number);
 
-  result = add_from_1_to_given_num(num);
-  printf("num= %d", result);
+  result = add_from_1_to_given_number(number);
+  printf("result= %d", result);
 
   return 0;
 }
