f74fc12fdf813cf83dda2d5723aeea5b1b6b9252
RoboStuido <speciers63@gmail.com>
2022-12-21 11:25:13 +0800

    New Class

---
+++ Weeks/Week-15/Class-23/main.c
@@ -0,0 +1,19 @@
+/*
+  請設計一C程式，可輸入A和B(A<B)，
+  列出A到B中，不可被3、4或5其中之一整除者。
+  (限用函數)
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void fun(int A, int B) {
+  for(int i=A; i<=B; i++) {
+    if(((i%3!=0)&&(i%4!=0)&&(i%5!=0))) {
+      printf("i=%3d\n", i);
+    }
+  }
+}
+
+int main() {
+  fun(10, 36);
+}
