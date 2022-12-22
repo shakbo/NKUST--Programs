952d7a0f7c52f1d206a6f89440fa9dcb8bb225cf
RoboStuido <speciers63@gmail.com>
2022-12-22 19:03:43 +0800

    Repair

--- Weeks/Week-12/AfterClass/Question-5/main.c
+++ Weeks/Week-12/AfterClass/Question-5/main.c
@@ -1,29 +1,24 @@
+// This is Work in Progress
+
 #include <stdio.h>
 #include <stdlib.h>
 
-int checkisprime(int cpn);
+int list_all_prime(int number) {
+  int count = 0;
+  for(int i=2; i<number; i++) {
+    for(int j=i; j<i/2; j++) {
+      if(i%j==0) printf("%d\n", i);
+    }
+  }
+  return 0;
+}
 
 int main() {
   int N;
   printf("請輸入數字: ");
   scanf("%d", &N);
 
-  int prime = checkisprime(N);
-  if(prime) printf("%d 是質數", N);
-  else if(!prime) printf("%d 不是質數", N);
+  list_all_prime(N);
 
   return 0;
-}
-
-int checkisprime(int cpn) {
-  int prime = 1;
-  if(cpn == 0 || cpn == 1) {
-    prime = 0;
-  }
-  for(int i=2; i<=cpn/2; ++i) {
-    if(cpn%i == 0) {
-      prime = 0;
-    }
-  }
-  return prime;
 }
