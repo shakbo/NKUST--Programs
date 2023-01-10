db39c2ce4df6f4728c89478d4bad701521277980
RoboStuido <speciers63@gmail.com>
2023-01-10 20:35:33 +0800

    Fix codes that cause warning

--- Extensions/Official/W6C12-2/main.c
+++ Extensions/Official/W6C12-2/main.c
@@ -1,53 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main() {
   int n=0;
-  int factorCounter=0; //宣告計數器並歸零
-  int EvenCounter=0; //宣告計數器並歸零
-  int OddCounter=0; //宣告計數器並歸零
-  
-  printf("請輸入一個整數 > ");
+  int factorCounter=0, evenCounter=0, oddCounter=0; //宣告計數器並歸零
+
+  printf("請輸入一個整數: ");
   scanf("%d", &n);
 
-  int EvenNums[n];
-  int OddNums[n];
+  int *evenNums, *oddNums;
+  evenNums = malloc(sizeof(int)*n);
+  oddNums = malloc(sizeof(int)*n);
 
-  for(int i=0; i<=n; i++) {
-    EvenNums[i] = 0;
-    OddNums[i] = 0;
+  for(int i=0; i<n; i++) {
+    evenNums[i] = 0;
+    oddNums[i] = 0;
   }
 
   for(int i=1; i<=n; i++) { // 檢查1到n中所有的數，哪些是n的因數。
     if(n%i==0) {
       if(i%2==0) { // 判斷因數以2除是否為0
-        EvenNums[EvenCounter] = i;
-        EvenCounter++;
+        evenNums[evenCounter] = i;
+        evenCounter++;
       } else {
-        OddNums[OddCounter] = i;
-        OddCounter++;
+        oddNums[oddCounter] = i;
+        oddCounter++;
       }
       factorCounter++;
     }
   }
-  printf("\n總共有 %d 個因數\n\n", factorCounter); //印出有幾個因數
 
-  int OddNumSize = (sizeof(OddNums)/sizeof(*OddNums));
-  int EvenNumSize = (sizeof(EvenNums)/sizeof(*EvenNums));
+  printf("\n共有 %d 個因數\n\n", factorCounter); //印出有幾個因數
 
-  printf("其中是奇數的有 %d 個，分別是： \n", OddCounter);
-  for(int i=0; i<=OddNumSize; i++) {
-    if(OddNums[i]==0) break;
-    else printf("%d\n", OddNums[i]);
+  int oddNumSize = (sizeof(oddNums)/sizeof(*oddNums));
+  int evenNumSize = (sizeof(evenNums)/sizeof(*evenNums));
+
+  printf("其中有 %d 個是奇數，有: \n", oddCounter);
+  for(int i=0; i<=oddNumSize; i++) {
+    if(oddNums[i]==0) break;
+    printf("%3d\n", oddNums[i]);
   }
 
   printf("\n\n");
 
-  printf("其中是偶數的有 %d 個，分別是： \n", EvenCounter);
-  for(int i=0; i<=EvenNumSize; i++) {
-    if(EvenNums[i]==0) break;
-    else printf("%d\n", EvenNums[i]);
+  printf("其中有 %d 個是偶數，有: \n", evenCounter);
+  for(int i=0; i<=evenNumSize; i++) {
+    if(evenNums[i]==0) break;
+    printf("%3d\n", evenNums[i]);
   }
 
   printf("\n\n");
-  system("pause");
+
+return 0;
 }
